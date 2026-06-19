441c3bd0c03975530c0b6dbd36911e06975f5816
sevans70-web <sevans70@hotmail.com>
2026-06-18 23:39:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -42,13 +42,13 @@ st.header("🚨 Best Bets Today")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.success("💣 HR Play\n\nAaron Judge")
+    st.success("💣 HR Play\n\nAaron Judge\n\nHRR Rating: 96")
 
 with col2:
-    st.success("🔥 Hits Play\n\nLuis Arraez")
+    st.success("🔥 Hits Play\n\nLuis Arraez\n\nHits Rating: 94")
 
 with col3:
-    st.success("⚾ TB Play\n\nShohei Ohtani")
+    st.success("⚾ TB Play\n\nShohei Ohtani\n\nTB Rating: 93")
 
 st.divider()
 # HR WATCHLIST
