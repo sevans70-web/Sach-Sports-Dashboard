a4a2c0d12a27699b14b4a1cd13ead0e839296bb9
sevans70-web <sevans70@hotmail.com>
2026-07-22 13:13:42 -0400

    Connect Player Intelligence page to engine

--- pages/player.py
+++ pages/player.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Player Intelligence",
+    player = get_player_profile(660271)
+
+st.write(player)
     page_icon="⚾",
     layout="wide",
 )
