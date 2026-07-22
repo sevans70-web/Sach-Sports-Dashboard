a9b3994963332b16aaa13ef6e9300df2f19abab2
sevans70-web <sevans70@hotmail.com>
2026-07-22 13:08:09 -0400

    Create Player Intelligence page

--- pages/player.py
+++ pages/player.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Player Intelligence",
+    page_icon="⚾",
+    layout="wide",
+)
+
+st.title("⚾ Player Intelligence")
+
+st.info("Player Intelligence page coming soon.")
