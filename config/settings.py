aed8468590d76104a698dc2e0a814304457b070e
sevans70-web <sevans70@hotmail.com>
2026-07-23 18:35:01 -0400

    Refactor settings to use environment variables
    Updated APP_ENVIRONMENT to use environment variable. Added SUPABASE_URL and SUPABASE_KEY to use environment variables.

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,11 @@
 Application settings for the Sach Sports Platform.
 """
 
+import os
+
 APP_NAME = "Sach Sports"
 APP_VERSION = "1.0.0"
-APP_ENVIRONMENT = "development"
+APP_ENVIRONMENT = os.getenv("APP_ENVIRONMENT", "development")
+
+SUPABASE_URL = os.getenv("SUPABASE_URL", "")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
