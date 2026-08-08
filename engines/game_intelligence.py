5b573d888064ad455e0723677ef6760ecf3389dd
sevans70-web <sevans70@hotmail.com>
2026-08-07 22:42:25 -0400

    Fix probability ranking block indentation

--- engines/game_intelligence.py
+++ engines/game_intelligence.py
@@ -924,11 +924,11 @@ def rank_players(
             }
         )
 
-probability_field = {
-    CATEGORY_HOME_RUNS: "home_run_probability",
-    CATEGORY_HITS: "one_plus_hit_probability",
-    CATEGORY_TOTAL_BASES: "over_1_5_total_bases_probability",
-}[category]
+    probability_field = {
+        CATEGORY_HOME_RUNS: "home_run_probability",
+        CATEGORY_HITS: "one_plus_hit_probability",
+        CATEGORY_TOTAL_BASES: "over_1_5_total_bases_probability",
+    }[category]
 
     scored_players.sort(
         key=lambda item: (
