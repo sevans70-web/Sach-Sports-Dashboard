2216254a592dd658fb9ee7becf161ef1af3ed937
sevans70-web <sevans70@hotmail.com>
2026-08-09 21:05:53 -0400

    Connect lineup data to ranking engine

--- engines/game_intelligence.py
+++ engines/game_intelligence.py
@@ -25,6 +25,7 @@ from typing import Any
 from zoneinfo import ZoneInfo
 
 from data.mlb_stats import get_today_hitters_with_stats
+from data.mlb_lineups import get_mlb_lineups
 
 from data.mlb_pitchers import get_today_probable_pitchers_with_stats
 from data.mlb_weather import get_game_weather
