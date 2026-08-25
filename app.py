7dcd567b4ce8a4ced974560c27c3aa1c55c3beea
sevans70-web <sevans70@hotmail.com>
2026-08-24 20:21:19 -0400

    Add mobile-first College Football foundation

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ pages = {
         st.Page("pages/home.py", title="Home", icon="🏠", default=True),
         st.Page("pages/mlb.py", title="MLB", icon="⚾"),
         st.Page("pages/nfl.py", title="NFL", icon="🏈"),
+        st.Page("pages/cfb.py", title="College Football", icon="🏈"),
         st.Page("pages/nba.py", title="NBA", icon="🏀"),
         st.Page("pages/nhl.py", title="NHL", icon="🏒"),
         st.Page("pages/soccer.py", title="Soccer", icon="⚽"),
@@ -20,4 +21,3 @@ pages = {
 
 navigation = st.navigation(pages)
 navigation.run()
-
